4bf1485e16ca11f819d107773992343afbcc196c
Ahmed Nawaz Gondal <nawaz07210721@gmail.com>
2024-09-29 17:22:23 +0500

    build error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjusts the warning threshold for chunk sizes
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "lodash"], // Separates these libraries into their own chunk
+        },
+      },
+    },
+  },
+};
